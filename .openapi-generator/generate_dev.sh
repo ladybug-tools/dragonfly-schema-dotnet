1f9860d6d2b4d5f22b4dae353f00051e8b529e37
MingboPeng <mingo1214@gmail.com>
2020-08-05 20:30:25 -0400

    chore(generate): remove reference_honeybee_schema.py

--- .openapi-generator/generate_dev.sh
+++ .openapi-generator/generate_dev.sh
@@ -11,4 +11,3 @@ python .openapi-generator/update_assembly_version.py
 python .openapi-generator/create_interface.py "https://www.ladybug.tools/dragonfly-schema/model_mapper.json"
 
 python .openapi-generator/remove_honeybee_schema.py "https://www.ladybug.tools/dragonfly-schema/model_mapper.json"
-python .openapi-generator/reference_honeybee_schema.py
