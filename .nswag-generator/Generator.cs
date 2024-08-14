b1bd25e960a98d946b2c78f14b56cf1f9d250a0f
Mingbo Peng <mingo1214@gmail.com>
2024-08-13 22:43:39 -0400

    fix(Generator): clean non-dragonfly files

--- .nswag-generator/Generator.cs
+++ .nswag-generator/Generator.cs
@@ -1,11 +1,14 @@
 ﻿using System;
+using Newtonsoft.Json.Linq;
+using System.Linq;
+using System.Collections.Generic;
 
 namespace SchemaGenerator;
 
 internal class Generator
 {
     private static readonly string _generatorFolder = ".nswag-generator";
-    public static string sdkName = "HoneybeeSchema";
+    public static string sdkName = "DragonflySchema";
     public static string workingDir = Environment.CurrentDirectory;
     public static string rootDir => workingDir.Substring(0, workingDir.IndexOf(_generatorFolder) + _generatorFolder.Length);
     static void Main(string[] args)
@@ -23,6 +26,35 @@ internal class Generator
 
         //GenTsDTO.Execute();
         GenCsDTO.Execute();
+
+        CleanHoneybeeSchema();
+
+    }
+
+    /// <summary>
+    /// Remove files that belongs to Honeybee Schema
+    /// </summary>
+    static void CleanHoneybeeSchema()
+    {
+        var docDir = System.IO.Path.Combine(System.IO.Path.GetDirectoryName(rootDir), ".openapi-docs");
+        var schemaFile = System.IO.Path.Combine(docDir, "model_mapper.json");
+        var json = System.IO.File.ReadAllText(schemaFile, System.Text.Encoding.UTF8);
+        Console.WriteLine($"Reading schema from {schemaFile}");
+        var jDocObj = JObject.Parse(json);
+
+        var classes = jDocObj["classes"] as JObject;
+        var enumns = jDocObj["enums"] as JObject;
+        var hbProps = classes.Properties().Where(_ => _.Value.ToString().StartsWith("honeybee_schema.")).Select(_ => _.Name);
+        var hbEnums = enumns.Properties().Where(_ => _.Value.ToString().StartsWith("honeybee_schema.")).Select(_ => _.Name);
+
+        var srcDir = System.IO.Path.Combine(System.IO.Path.GetDirectoryName(rootDir), "src", sdkName, "Model");
+        var filesTobeRemoved = hbProps.Concat(hbEnums).Select(_ => _.StartsWith("_") ? _.Substring(1) : _).Select(_ => System.IO.Path.Combine(srcDir, _ + ".cs")).ToList();
+        foreach (var item in filesTobeRemoved)
+        {
+            System.IO.File.Delete(item);
+        }
+
+        //props == 
 
     }
 
