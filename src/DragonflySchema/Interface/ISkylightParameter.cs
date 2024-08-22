baf088edea2e407f2bdaa5a463232ceeecffec07
Mingbo Peng <mingo1214@gmail.com>
2024-08-21 23:48:40 -0400

    fix(schema): regenerate schema

---
+++ src/DragonflySchema/Interface/ISkylightParameter.cs
@@ -0,0 +1,19 @@
+﻿// Auto-generated
+
+namespace DragonflySchema
+{
+	public partial interface ISkylightParameter {}
+}
+
+
+//Classes implemented this interface:
+namespace DragonflySchema
+{
+	
+	public partial class DetailedSkylights: DragonflySchema.ISkylightParameter {}
+	
+	public partial class GriddedSkylightArea: DragonflySchema.ISkylightParameter {}
+	
+	public partial class GriddedSkylightRatio: DragonflySchema.ISkylightParameter {}
+	
+}
---
+++ src/DragonflySchema/Interface/Radiance/IProperties.cs
@@ -0,0 +1,23 @@
+﻿// Auto-generated
+
+namespace DragonflySchema.Radiance
+{
+	public partial interface IProperties {}
+}
+
+
+//Classes implemented this interface:
+namespace DragonflySchema
+{
+	
+	public partial class BuildingRadiancePropertiesAbridged: DragonflySchema.Radiance.IProperties {}
+	
+	public partial class ContextShadeRadiancePropertiesAbridged: DragonflySchema.Radiance.IProperties {}
+	
+	public partial class ModelRadianceProperties: DragonflySchema.Radiance.IProperties {}
+	
+	public partial class Room2DRadiancePropertiesAbridged: DragonflySchema.Radiance.IProperties {}
+	
+	public partial class StoryRadiancePropertiesAbridged: DragonflySchema.Radiance.IProperties {}
+	
+}
