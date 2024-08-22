ecc5c4990a770a32bb16713990480e4df4b72a3b
Mingbo Peng <mingo1214@gmail.com>
2024-08-21 23:47:23 -0400

    fix(generator): update generator

--- .nswag-generator/Generator.cs
+++ .nswag-generator/Generator.cs
@@ -1,7 +1,8 @@
 ﻿using System;
-using Newtonsoft.Json.Linq;
 using System.Linq;
-using System.Collections.Generic;
+using System.Text;
+using Newtonsoft.Json.Linq;
+using System.Text.RegularExpressions;
 
 namespace SchemaGenerator;
 
@@ -24,8 +25,30 @@ public partial class Generator
         var outputDir = System.IO.Path.Combine(rootDir, "Output");
         System.IO.Directory.CreateDirectory(outputDir);
 
-        //GenTsDTO.Execute();
-        GenCsDTO.Execute();
+        if (args == null || !args.Any())
+            args = new string[] { "--download", "--genTsModel", "--genCsModel", "--genCsInterface", "--updateVersion" };
+
+
+        // download all json files
+        if (args.Contains("--download"))
+        {
+            HttpHelper.SetUp();
+            GetSchemaJsonFiles();
+        }
+
+        if (args.Contains("--genCsModel"))
+        {
+            GenCsDTO.Execute();
+        }
+
+        //Generate Interfaces
+        if (args.Contains("--genCsInterface"))
+        {
+            GenInterface.Execute();
+        }
+
+        if (args.Contains("--updateVersion"))
+            UpdateVersions();
 
         CleanHoneybeeSchema();
 
@@ -55,6 +78,87 @@ public partial class Generator
         }
 
         //props == 
+
+    }
+
+
+    // Download all schema jsons
+    static void GetSchemaJsonFiles()
+    {
+        var baseURL = @"https://www.ladybug.tools/dragonfly-schema";
+        var files = new string[] {
+            "model_inheritance.json",
+            "model_mapper.json",
+            };
+
+        var dir = System.IO.Path.Combine(System.IO.Path.GetDirectoryName(rootDir), ".openapi-docs");
+        if (System.IO.Directory.Exists(dir))
+            System.IO.Directory.Delete(dir, true);
+        System.IO.Directory.CreateDirectory(dir);
+        foreach (var f in files)
+        {
+            var url = $"{baseURL}/{f}";
+            var saveAs = System.IO.Path.Combine(dir, f);
+            var savedAs = HttpHelper.DownloadFile(url, saveAs);
+            if (!System.IO.File.Exists(savedAs))
+                throw new ArgumentException($"Failed to rename file to {saveAs}");
+        }
+
+    }
+
+    public static void UpdateVersions()
+    {
+        // get the current version from model_inheritance.json
+        var root = System.IO.Path.GetDirectoryName(rootDir);
+        var docDir = System.IO.Path.Combine(root, ".openapi-docs");
+        var jsonFile = System.IO.Path.Combine(docDir, "model_inheritance.json");
+        var modelJson = System.IO.File.ReadAllText(jsonFile);
+        var newVersion = JObject.Parse(modelJson)["info"]["version"].ToString();
+        newVersion = string.IsNullOrEmpty(newVersion) ? "1.0.0" : newVersion;
+
+        var packageName = sdkName;
+
+        // Check the version from nuget
+        var api = $"https://api.nuget.org/v3-flatcontainer/{sdkName.ToLower()}/index.json";
+        var versions = HttpHelper.ReadJson(api)["versions"] as JArray;
+        if (versions != null && versions.Any())
+        {
+            var lastVersion = versions.Last().ToString();
+            Console.WriteLine($"Found latest version on Nuget: {lastVersion}");
+            if (lastVersion.StartsWith(newVersion))
+                newVersion = lastVersion;
+            else
+                Console.WriteLine($"Schema version {newVersion} is newer than the latest version on Nuget: {lastVersion}");
+
+        }
+
+        Console.WriteLine($"Getting an existing version: {newVersion}");
+        var digits = newVersion.Split(new[] { '.', '-' });
+        if (digits.Length == 3)
+        {
+            newVersion = $"{newVersion}-v1";
+        }
+        else
+        {
+            var lastV = digits.LastOrDefault().Replace("v", "");
+            var v = int.Parse(lastV) + 1;
+            newVersion = string.Join(".", digits.SkipLast(1)) + $"-v{v}";
+        }
+        Console.WriteLine($"New version: {newVersion}");
+
+
+        //# update the version for CSharp
+        var assemblyFile = System.IO.Path.Combine(root, "src", packageName, $"{packageName}.csproj");
+        var file = System.IO.File.ReadAllText(assemblyFile);
+        var newFile = Regex.Replace(file, @"(?<=\SVersion\>)\S+(?=\<\/)", newVersion);
+        System.IO.File.WriteAllText(assemblyFile, newFile, Encoding.UTF8);
+
+
+        //# update the version for TypeScript
+        var tsFile = System.IO.Path.Combine(root, "src", "TypeScriptSDK", "package.json");
+        file = System.IO.File.ReadAllText(tsFile);
+        newFile = Regex.Replace(file, @"(?<=version"": "")[^""]+(?="")", newVersion);
+        System.IO.File.WriteAllText(tsFile, newFile, Encoding.UTF8);
 
     }
 
